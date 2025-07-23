4c7c6cc63a470b99bcbee551240ec4cdf826b538
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-07-23 22:30:41 +0530

    Auth

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,33 @@
+import userModel from '../models/userModel.js';
+
+// Add an item to the user's cart
+const addToCart = async (req, res) => {
+    try {
+        let userData = await userModel.findOne({ _id:req.body.userId });
+        let cartData = await userData.cartData;
+        if (!cartData[req.body.itemId]) {
+            cartData[req.body.itemId] = 1
+        }
+        else{
+            cartData[req.body.itemId] += 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData})
+        res.json({ success: true, message: "Item added to cart" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error adding item to cart" });
+    }
+}
+
+
+// remove an item from the user's cart
+const removeFromCart = async (req, res) => {
+
+}
+
+// Get the user's cart
+const getCart = async (req, res) => {
+
+}
+
+export { addToCart, removeFromCart, getCart };
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from './config/db.js';
 import itemRouter from './routes/itemRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config.js'; // to load environment variables
+import cartRouter from './routes/cartRoute.js';
 
 
 // Add config
@@ -21,6 +22,7 @@ connectDB();
 app.use('/api/item', itemRouter);    // to check 
 app.use("/images",express.static("uploads"))
 app.use('/api/user', userRouter);
+app.use('/api/cart', cartRouter);
 
 app.get("/", (req, res) => {
     res.send("API is running...")
