bfab63dbe29aa3f82e086916187a259eb937fb1d
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-07-24 21:49:29 +0530

    Error remove

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import { connectDB } from './config/db.js';
 import itemRouter from './routes/itemRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config.js'; // to load environment variables
-import cartRouter from './routes/cartRoute.js';
 
 
 // Add config
@@ -22,7 +21,6 @@ connectDB();
 app.use('/api/item', itemRouter);    // to check 
 app.use("/images",express.static("uploads"))
 app.use('/api/user', userRouter);
-app.use('/api/cart', cartRouter);
 
 app.get("/", (req, res) => {
     res.send("API is running...")
